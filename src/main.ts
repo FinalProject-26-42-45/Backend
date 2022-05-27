f05e26ce396dddadc437fa1634e6ddbd7ee00daf
Nattima Mahapan <nattima0912@gmail.com>
2022-05-27 13:40:21 +0700

    [UPDATE] dev

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:8080'],
+    origin: ['http://localhost:8080', 'https://foodrand.hopto.org'],
     methods: ['POST', 'PUT', 'GET', 'DELETE']
   })
   app.use(express.static('images'))
