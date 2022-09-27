18395785e2c921c1c6c5047a81021b9dab059048
Nattima Mahapan <nattima0912@gmail.com>
2022-09-27 13:53:29 +0700

    EditUser and MenuHistory

--- src/controller/menuhistory/menuhistory.controller.ts
+++ src/controller/menuhistory/menuhistory.controller.ts
@@ -1,10 +1,7 @@
 import { Controller, Get, Post, Body, Param, Delete, Put, UseGuards, Request } from '@nestjs/common';
 import { MenuhistoryService } from '../../services/menuhistory/menuhistory.service';
-// import { CreateMenuhistoryDto } from '../../dto/menuhistory/create-menuhistory.dto';
-// import { UpdateMenuhistoryDto } from '../../dto/menuhistory/update-menuhistory.dto';
 import { Menuhistory } from '../../entities/menuhistory.entity';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { log } from 'console';
 
 @Controller('menuhistory')
 export class MenuhistoryController {
@@ -12,7 +9,12 @@ export class MenuhistoryController {
 
   @UseGuards(JwtAuthGuard)
   @Post()
-  addMenuhistory(@Request() request: any, @Body() Menuname: any){
-    this.menuhistoryService.addHistory(request.user.UserId, Menuname.MenuName)
+  addMenuhistory(@Request() request: any, @Body() MenuName: any){
+    this.menuhistoryService.addHistory(request.user.UserId, MenuName.MenuName)
+  }
+
+  @Delete(":HistoryId")
+  async remove(@Param() HistoryId: number){
+    return this.menuhistoryService.deleteHistory(HistoryId)
   }
 }
--- src/controller/users/users.controller.ts
+++ src/controller/users/users.controller.ts
@@ -1,33 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, UseInterceptors, Request } from '@nestjs/common';
 import { UsersService } from '../../services/users/users.service';
 import { Users } from 'src/entities/users.entity';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { upload } from 'src/services/common/common.service';
 
 @Controller('users')
 export class UsersController {
+
   constructor(private usersService: UsersService) {}
 
-  // @Get()
-  // async findAll() {
-  //   return await this.usersService.findAll();
-  // }
+  @Get()
+  async findAll() {
+    return await this.usersService.findAll()
+  }
 
-  // @Get(':UserId')
-  // async findOne(@Param() UserId: number) {
-  //   return await this.usersService.findOne(UserId);
-  // }
+  @Get(":UserId")
+  async findOne(@Param() UserId: number){
+    return this.usersService.findUser(UserId)
+  }
 
-  // @Post()
-  // async create(@Body() createUserDto: Users) {
-  //   return await this.usersService.create(createUserDto);
-  // }
-
-  // @Put(":UserId")
-  // async update(@Param() UserId: number, @Body() createUserDto: Users) {
-  //   return this.usersService.update(UserId, createUserDto);
-  // }
-
-  // @Delete()
-  // async delete(@Body() UserId: number) {
-  //   return this.usersService.remove(UserId);
-  // }
+  @Put()
+  @UseInterceptors(
+    FileFieldsInterceptor([{ name: 'json', maxCount: 1 }], upload))
+  updateUser() {
+    return this.usersService.editUser()
+  }
 }
